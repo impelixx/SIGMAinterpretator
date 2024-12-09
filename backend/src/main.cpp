0d3ef31834c42a666e0ac72f5a4526282f8851c2
anton <helloimgood@list.ru>
2024-12-09 23:33:54 +0300

    fix arguments to run programm

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -7,37 +7,35 @@
 
 int main(int argc, char* argv[]) {
     if (argc < 3) {
-        std::cerr << "Usage: " << argv[0] << " <input_file> <workword_file>" << std::endl;
-        return 1;
+        if (argc != 1) {
+            std::cerr << "Usage: " << argv[0] << " <path to code file> <path to workwords file>" << std::endl;
+            return 1;
+        }
     }
 
-    std::string code;
-    std::string workwords;
+    std::string code = "";
+    std::string workwords = "";
 
     try {
-        if (argc < 3) {
-            std::ifstream codeFile("../test/code.us");
-            std::ifstream workwordFile("../test/keywords");
-            if (!codeFile.is_open()) {
-                throw std::runtime_error("Failed to open code file '../test/code.us'");
-            }
-            if (!workwordFile.is_open()) {
-                throw std::runtime_error("Failed to open workword file '../test/keywords'");
-            }
-            std::stringstream codeBuffer;
-            codeBuffer << codeFile.rdbuf();
-            code = codeBuffer.str();
-
+        std::ifstream codeFile;
+        if (argc != 1) {
+            codeFile = std::ifstream(std::string(argv[1]));
+        } else {
+            codeFile = std::ifstream("../test/code.us");
         }
-        std::ifstream codeFile(argv[1]);
         if (!codeFile.is_open()) {
             throw std::runtime_error(std::string("Failed to open code file '") + argv[1] + "'");
         }
         std::stringstream codeBuffer;
         codeBuffer << codeFile.rdbuf();
         code = codeBuffer.str();
-
-        LexemAnalyzer analyzer(code, argv[2]);
+        std::ifstream workwordFile;
+        if (argc != 1) {
+            workwords = std::string(argv[2]);
+        } else {
+            workwords = "../test/workword";
+        }
+        LexemAnalyzer analyzer(code, workwords);
 
         try {
             analyzer.Analyze();
