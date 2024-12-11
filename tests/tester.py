1858eecdd3079fabfc89379597bc7570517564e5
anton <helloimgood@list.ru>
2024-12-11 22:08:24 +0300

    add: test for all programm

--- tests/tester.py
+++ tests/tester.py
@@ -5,9 +5,9 @@ def run_test(command):
     result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return result
 
-def lexerTest():
+def Test():
     print("Running lexers tests")
-    for file in sorted(os.listdir("lexerTests")):
+    for file in sorted(os.listdir("tests")):
         print(f"Testing {file}")
         result = run_test(f"./tests/backend tests/lexerTests/{file}/code.us tests/lexerTests/{file}/workword")
         if result.returncode == 1:
@@ -17,17 +17,5 @@ def lexerTest():
         else:
             print("Test passed")
 
-def semanticTest():
-    print("Running semantic tests")
-    for file in reversed(os.listdir("tests/semanticTests")):
-        print(f"Testing {file}")
-        result = run_test(f"./tests/backend tests/semanticTests/{file}/code.us tests/semanticTests/{file}/workword")
-        if result == 2:
-            print("Test failed")
-            exit(1)
-        else:
-            print("Test passed")
-
 if __name__ == "__main__":
-    lexerTest()
-    # semanticTest()
+    Test()
