a6e83dc19c6fd5996b9c981fd70d1ae73b746a7a
impelixx <119325773+impelixx@users.noreply.github.com>
2025-01-11 09:11:23 +0000

    [MegaLinter] Apply linter fixes

--- backend/src/Lexem.cpp
+++ backend/src/Lexem.cpp
@@ -12,8 +12,7 @@ std::string Lexem::get_type() const {
   if (type_ == LexemType::NUMBER) {
     return "NUMBER";
   }
-  if (type_ == LexemType::EOS)
-  {
+  if (type_ == LexemType::EOS) {
     return "EOS";
   }
   if (type_ == LexemType::OPERATOR) {
